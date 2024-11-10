01245122b5185115045dc8a10c768cd66e1a84e7
deezznuttzz <danieldavel22@gmail.com>
2024-11-10 10:39:57 +0200

    ffalsave_ofdemo

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
+      
       ignoreBuildErrors: true, // Optional: Skips TypeScript errors during builds
     },
+    
     outputFileTracing: true, // Helps cache server-side files
-  };
+
+    distDir: 'build',
+
+
+  }
+  
+
   
   export default nextConfig;
   
